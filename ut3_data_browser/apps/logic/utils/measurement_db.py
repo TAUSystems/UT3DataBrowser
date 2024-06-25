d00164a2d474ed3616604d9f89877449f96845df
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-06-25 15:04:28 -0700

    Update utils/measurement_db.py to use config

---
+++ ut3_data_browser/apps/logic/utils/measurement_db.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy import MetaData, create_engine, URL
+
+from ..settings import load_config
+
+if TYPE_CHECKING:
+    from sqlalchemy.schema import Table
+
+def get_sqlalchemy_engine():
+    config = load_config()
+    measurement_db_config = config['measurement_db']
+
+    return create_engine(URL.create(config.get('MEASUREMENT_DB_SQLALCHEMY_DRIVER'),
+                            host=measurement_db_config.get('MEASUREMENT_DB_HOST'),
+                            port=measurement_db_config.get('MEASUREMENT_DB_PORT'),  
+                            database=measurement_db_config.get('MEASUREMENT_DB_DBNAME'),
+                            username=measurement_db_config.get('MEASUREMENT_DB_USERNAME'),
+                            password=measurement_db_config.get('MEASUREMENT_DB_PASSWORD'),
+                        ))
+
+def get_scan_table() -> Table:
+    sqlalchemy_engine = get_sqlalchemy_engine()
+
+    measurement_db_metadata = MetaData()
+    measurement_db_metadata.reflect(sqlalchemy_engine)
+
+    scan_table  = measurement_db_metadata.tables['scan']
+
+    return scan_table
+
