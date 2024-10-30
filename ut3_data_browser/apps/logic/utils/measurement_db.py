b94b2de7d6f5eb2dadfef851451227376d21c49a
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-10-30 11:35:17 -0700

    Make measurement_db_engine a global object
    Previously, sqlalchemy_engine and tables were module-level objects in
    scans.py. Thus there was no way to update them if for example the
    database settings changed.
    
    This commit creates a MeasurementDBEngine class and an instance
    measurement_db_engine in measurement_db.py, which holds the sqlalchemy
    engine and tables and provides methods to update them.
    
    It updates functions in scans.py to use this object.

--- ut3_data_browser/apps/logic/utils/measurement_db.py
+++ ut3_data_browser/apps/logic/utils/measurement_db.py
@@ -9,24 +9,68 @@ from ...types import ConfigurationDict, MeasurementDBConfigurationDict
 
 if TYPE_CHECKING:
     from sqlalchemy.schema import Table
+    from sqlalchemy.engine.base import Engine as SQLAlchemyEngine
 
-def get_sqlalchemy_engine():
-    config: ConfigurationDict = load_config()
-    measurement_db_config: MeasurementDBConfigurationDict = config['measurement_db']
+class MeasurementDBEngine:
+    """ Class to manage the SQLAlchemy engine and tables for the measurement database 
+    
+    Meant to be used as a global instance. 
 
-    return create_engine(URL.create(measurement_db_config.get('driver'),
-                            host=measurement_db_config.get('host'),
-                            port=measurement_db_config.get('port'),  
-                            database=measurement_db_config.get('dbname'),
-                            username=measurement_db_config.get('username'),
-                            password=measurement_db_config.get('password'),
-                        ))
+    Properties:
+    -----------
+    sqlalchemy_engine : SQLAlchemyEngine
+    tables : dict[str, Table]
 
-def get_tables() -> dict[str, Table]:
-    sqlalchemy_engine = get_sqlalchemy_engine()
+    """
+    def __init__(self):
+        self._sqlalchemy_engine: SQLAlchemyEngine = None
+        self._tables: dict[str, Table] = {}
+        self.load()
 
-    measurement_db_metadata = MetaData()
-    measurement_db_metadata.reflect(sqlalchemy_engine)
+    def load(self):
+        self.create_engine()
+        self.get_tables()
 
-    return measurement_db_metadata.tables
+    @property
+    def sqlalchemy_engine(self) -> SQLAlchemyEngine:
+        if not self._sqlalchemy_engine:
+            self.create_engine()
+        return self._sqlalchemy_engine
 
+    @property
+    def tables(self) -> dict[str, Table]:
+        if not self._tables:
+            self.get_tables()
+        return self._tables
+
+    def create_engine(self):
+        config: ConfigurationDict = load_config()
+        measurement_db_config: MeasurementDBConfigurationDict = config['measurement_db']
+
+        try:
+            self._sqlalchemy_engine = create_engine(URL.create(
+                drivername=measurement_db_config.get('driver'),
+                host=measurement_db_config.get('host'),
+                port=measurement_db_config.get('port'),  
+                database=measurement_db_config.get('dbname'),
+                username=measurement_db_config.get('username'),
+                password=measurement_db_config.get('password'),
+            ))
+
+        except Exception as err:
+            print(f"Error creating SQLAlchemy engine: {err}")
+            self._sqlalchemy_engine = None
+
+    def get_tables(self):
+        if not self._sqlalchemy_engine:
+            self.create_engine()
+        
+        if not self._sqlalchemy_engine:
+            self._tables = {}
+        
+        measurement_db_metadata = MetaData()
+        measurement_db_metadata.reflect(self._sqlalchemy_engine)
+
+        self._tables = measurement_db_metadata.tables
+
+measurement_db_engine = MeasurementDBEngine()
