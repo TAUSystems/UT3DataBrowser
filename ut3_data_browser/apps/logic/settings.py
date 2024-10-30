e30e25a54cd8d8989351d3c7966e888441a2ee89
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2024-10-30 02:16:50 +0000

    Remove any whitespace characters when saving variables_shown to config

--- ut3_data_browser/apps/logic/settings.py
+++ ut3_data_browser/apps/logic/settings.py
@@ -83,7 +83,7 @@ class UserDataConfiguration(Configuration):
         config = ConfigurationDict({})
 
         config['scalars'] = {
-            'variables_shown': [var for var in form_data['variables_shown'].split('\n') if var],
+            'variables_shown': [var.strip() for var in form_data['variables_shown'].split('\n') if var],
         }
 
         config['directories'] = {
